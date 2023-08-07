79371efa4fc018db2348202ac097637469b322bd
xvpowerg <c911010083@gmail.com>
2023-08-07 22:13:19 +0800

    Add files via upload

---
+++ 20230807/modu.py
@@ -0,0 +1,12 @@
+class test1:
+    def __init__(self):
+        print("Create")
+def fun1():
+    print("func1")
+if __name__ == '__main__': 
+       print("testmu!")
+"""
+__name__如果import 則__name__會是module名稱 目前範例是modu
+__name__ 如果這個python檔案直接被執行 則是__main__
+
+"""
